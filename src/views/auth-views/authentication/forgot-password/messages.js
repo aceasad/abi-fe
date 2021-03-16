faede37bccabe4a78bd02a5cd7adc7dcc4b7061f
gligorevic <igor.gligorevic@hotmail.com>
2021-03-16 10:18:55 +0100

    Aeva names to Abi

--- src/views/auth-views/authentication/forgot-password/messages.js
+++ src/views/auth-views/authentication/forgot-password/messages.js
@@ -5,18 +5,18 @@ export const scope = "forgot_password_page";
 export default defineMessages({
   forgottenPasswordTitle: {
     id: `${scope}.text.forgotten_password_title`,
-    defaultMessage: "Forgotten password",
+    defaultMessage: "Forgotten password"
   },
   forgotPasswordParagraph: {
     id: `${scope}.text.forgotten_password_paragraph`,
-    defaultMessage: `Please enter the email you use to sign in to Aeva. We'll then send you an email with the instructions to follow.`,
+    defaultMessage: `Please enter the email you use to sign in to Abi. We'll then send you an email with the instructions to follow.`
   },
   privacyPolicyLink: {
     id: `${scope}.link.privacy_policy`,
-    defaultMessage: "Privacy policy",
+    defaultMessage: "Privacy policy"
   },
   termsAndConditionsLink: {
     id: `${scope}.link.terms_and_conditions`,
-    defaultMessage: "Terms and conditions",
-  },
+    defaultMessage: "Terms and conditions"
+  }
 });
