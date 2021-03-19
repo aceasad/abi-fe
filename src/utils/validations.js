903ec0c9db5e48203621ab212eb5f2b077754d16
majakesin <kesin.maja@gmail.com>
2021-03-19 08:40:00 +0100

    #Created reset-password functionality

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -8,3 +8,11 @@ export const loginSchema = Yup.object().shape({
 export const forgotPasswordSchema = Yup.object().shape({
   email: Yup.string().email().required(),
 });
+
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+export const resetPasswordSchema = Yup.object().shape({
+  password: Yup.string().required().matches(passwordRegex),
+  passwordRepeat: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password")]),
+});
