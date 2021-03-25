5d35706250366ebad9af2c01d5438360e203c7ad
Slobodan Subotic <slobodan.subotic@vivifyideas.com>
2021-03-25 12:24:41 +0100

    #FOEK-53 - create clinic css modified.

--- src/components/shared-components/Form/FormImageField.js
+++ src/components/shared-components/Form/FormImageField.js
@@ -13,32 +13,29 @@ const FormImageUpload = ({ field, form, label }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [field.value]);
   return (
-    <div>
-      <div>
-        <input
-          id={field.name}
-          type="file"
-          accept="image/*"
-          name={field.name}
-          onChange={(e) => {
-            form.setFieldValue(field.name, e.target.files[0]);
-            e.target.value = null;
-          }}
-        />
+    <>
+      <input
+        id={field.name}
+        type="file"
+        accept="image/*"
+        name={field.name}
+        onChange={(e) => {
+          form.setFieldValue(field.name, e.target.files[0]);
+          e.target.value = null;
+        }}
+        className="mb-2"
+      />
+      <label htmlFor={field.name}>
+        {label}
         <div>
-          <label htmlFor={field.name}>
-            {label}
-            <div>
-              {imagePreview ? (
-                <img src={imagePreview} alt="Upload" />
-              ) : (
-                <UserOutlined />
-              )}
-            </div>
-          </label>
+          {imagePreview ? (
+            <img src={imagePreview} alt="Upload" style={{ height: '5rem' }} />
+          ) : (
+            <UserOutlined />
+          )}
         </div>
-      </div>
-    </div>
+      </label>
+    </>
   );
 };
 export default FormImageUpload;
--- src/views/app-views/ClinicPage/index.js
+++ src/views/app-views/ClinicPage/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Form, Input, Radio } from 'antd';
+import { Button, Form, Input, Radio, Row, Col } from 'antd';
 import { Formik, Field } from 'formik';
 import { clinicSchema } from 'utils/validations';
 import { useDispatch } from 'react-redux';
@@ -10,6 +10,7 @@ import FormImageField from 'components/shared-components/Form/FormImageField';
 import { useIntl } from 'react-intl';
 import { updateClinic } from 'redux/actions/Clinic';
 import { NO, FREE, AVAILABLE } from '../../../constants/ClinicConstants';
+import { MinusOutlined } from '@ant-design/icons';
 
 const ClinicPage = () => {
   const dispatch = useDispatch();
@@ -50,100 +51,142 @@ const ClinicPage = () => {
       >
         {({ setFieldValue, dirty, isValid, values, handleSubmit }) => (
           <Form layout="vertical" name="clinic-form" onSubmit={handleSubmit}>
-            <Field component={FormImageField} name={'photo'}></Field>
+            <Row className="mb-4">
+              <Col>
+                <Field component={FormImageField} name={'photo'}></Field>
+              </Col>
+            </Row>
 
-            <Field
-              component={FormField}
-              label={formatMessage(messages.clinic_name)}
-              name={'name'}
-              errorTexts={{
-                label: formatMessage(messages.error_input_label_name),
-              }}
-              autoFocus
-            />
-            <Field
-              component={FormField}
-              label={formatMessage(messages.phone_number)}
-              name={'phone_number'}
-              errorTexts={{
-                label: formatMessage(messages.error_input_label_phone_number),
-              }}
-              autoFocus
-            />
-            <Field
-              component={FormField}
-              label={formatMessage(messages.address)}
-              name={'address'}
-              errorTexts={{
-                label: formatMessage(messages.error_input_label_address),
-              }}
-              autoFocus
-            />
-            <Field
-              component={FormField}
-              label={formatMessage(messages.google_maps_link)}
-              name={'google_maps_link'}
-              errorTexts={{
-                label: formatMessage(
-                  messages.error_input_label_google_maps_link
-                ),
-              }}
-              autoFocus
-            />
-            <label>{formatMessage(messages.parking_availability)}</label>
-            <Form.Item name="radio-group">
-              <Radio.Group
-                onChange={(event) => {
-                  setVisibility(event.target.value === AVAILABLE);
-                  values.parking_availability = event.target.value;
-                }}
-              >
-                <Radio value={NO}>{formatMessage(messages.parking_no)}</Radio>
-                <Radio value={FREE}>
-                  {formatMessage(messages.parking_free)}
-                </Radio>
-                <Radio value={AVAILABLE}>
-                  {formatMessage(messages.parking_available)}
-                </Radio>
+            <Row>
+              <Col span={24}>
+                <Field
+                  component={FormField}
+                  label={formatMessage(messages.clinic_name)}
+                  name={'name'}
+                  errorTexts={{
+                    label: formatMessage(messages.error_input_label_name),
+                  }}
+                  autoFocus
+                />
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={8}>
+                <Field
+                  component={FormField}
+                  label={formatMessage(messages.phone_number)}
+                  name={'phone_number'}
+                  errorTexts={{
+                    label: formatMessage(
+                      messages.error_input_label_phone_number
+                    ),
+                  }}
+                  autoFocus
+                />
+              </Col>
+              <Col span={16}>
+                <Field
+                  component={FormField}
+                  label={formatMessage(messages.address)}
+                  name={'address'}
+                  errorTexts={{
+                    label: formatMessage(messages.error_input_label_address),
+                  }}
+                  autoFocus
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <Field
+                  component={FormField}
+                  label={formatMessage(messages.google_maps_link)}
+                  name={'google_maps_link'}
+                  errorTexts={{
+                    label: formatMessage(
+                      messages.error_input_label_google_maps_link
+                    ),
+                  }}
+                  autoFocus
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={18}>
+                <Form.Item
+                  name="radio-group"
+                  label={formatMessage(messages.parking_availability)}
+                >
+                  <Radio.Group
+                    onChange={(event) => {
+                      setVisibility(event.target.value === AVAILABLE);
+                      values.parking_availability = event.target.value;
+                    }}
+                  >
+                    <Radio value={NO}>
+                      {formatMessage(messages.parking_no)}
+                    </Radio>
+                    <Radio value={FREE}>
+                      {formatMessage(messages.parking_free)}
+                    </Radio>
+                    <Radio value={AVAILABLE}>
+                      {formatMessage(messages.parking_available)}
+                    </Radio>
+                  </Radio.Group>
+                </Form.Item>
+              </Col>
+              <Col span={6}>
                 {visibilityOfParkinSizeField ? (
-                  <div style={{ marginLeft: '330px' }}>
+                  <Form.Item label={formatMessage(messages.parking_size)}>
                     <Field
                       component={FormInputField}
-                      label={formatMessage(messages.parking_size)}
                       name={'parking_size'}
                       type={'number'}
                       min={1}
                       autoFocus
                     />
-                  </div>
+                  </Form.Item>
                 ) : null}
-              </Radio.Group>
+              </Col>
+            </Row>
+            <Form.Item label={formatMessage(messages.working_hours)}>
+              <Row gutter={8}>
+                <Col xs={10} md={6}>
+                  <Field
+                    component={FormInputField}
+                    name={'start_of_work'}
+                    type={'time'}
+                    autoFocus
+                  />
+                </Col>
+                <Col md={4} md={1} className="text-center">
+                  <MinusOutlined className="mt-3 text-primary" />
+                </Col>
+                <Col xs={10} md={6}>
+                  <Field
+                    component={FormInputField}
+                    name={'end_of_work'}
+                    type={'time'}
+                    autoFocus
+                  />
+                </Col>
+              </Row>
             </Form.Item>
-            <label>{formatMessage(messages.working_hours)}</label>
-            <div className="row">
-              <Field
-                component={FormInputField}
-                name={'start_of_work'}
-                type={'time'}
-                autoFocus
-              />
-              <Field
-                component={FormInputField}
-                name={'end_of_work'}
-                type={'time'}
-                autoFocus
-              />
-            </div>
-            <Form.Item>
-              <Button
-                name="create"
-                type="primary"
-                disabled={!dirty || !isValid}
-                onClick={() => handleSubmit(values)}
-              >
-                Create
-              </Button>
-            </Form.Item>
+
+            <Row>
+              <Col>
+                <Form.Item>
+                  <Button
+                    name="create"
+                    type="primary"
+                    disabled={!dirty || !isValid}
+                    onClick={() => handleSubmit(values)}
+                  >
+                    Create
+                  </Button>
+                </Form.Item>
+              </Col>
+            </Row>
           </Form>
         )}
       </Formik>
