8c8a1185e4bc64a211dd7645fa0280fcbdbd78f4
Slobodan Subotic <slobodan.subotic@vivifyideas.com>
2021-03-30 10:14:45 +0200

    #FOEK-53 - create clinic css corrections in progress.

--- src/views/app-views/ClinicPage/index.js
+++ src/views/app-views/ClinicPage/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Form, Radio, Row, Col, Card } from 'antd';
+import { Button, Form, Radio, Row, Col, Card, Typography } from 'antd';
 import { Formik, Field } from 'formik';
 import { clinicSchema } from 'utils/validations';
 import { useDispatch } from 'react-redux';
@@ -11,6 +11,9 @@ import { useIntl } from 'react-intl';
 import { updateClinic } from 'redux/actions/Clinic';
 import { NO, FREE, AVAILABLE } from '../../../constants/ClinicConstants';
 import { MinusOutlined } from '@ant-design/icons';
+import Layout, { Content, Header } from 'antd/lib/layout/layout';
+
+const { Title } = Typography;
 
 const ClinicPage = () => {
   const dispatch = useDispatch();
@@ -19,159 +22,187 @@ const ClinicPage = () => {
   const { formatMessage } = useIntl();
 
   return (
-    <Card className="p-3">
-      <Formik
-        initialValues={{
-          photo: null,
-          name: '',
-          phone_number: '',
-          address: '',
-          google_maps_link: '',
-          parking_availability: null,
-          parking_size: 0,
-          start_of_work: null,
-          end_of_work: null,
-        }}
-        validationSchema={clinicSchema}
-        onSubmit={(values) => {
-          dispatch(updateClinic(values));
-        }}
-      >
-        {({ dirty, isValid, values, handleSubmit }) => (
-          <Form layout="vertical" name="clinic-form" onSubmit={handleSubmit}>
-            <Row justify="center" className="mb-5 mt-4">
-              <Col span={6}>
-                <Field component={FormImageUpload} name={'photo'}></Field>
-              </Col>
-            </Row>
+    <Layout>
+      <Header className="ant-layout-page-header shadow-sm d-flex justify-content-sm-between">
+        <Title className="mb-sm-0">Create Clinic</Title>
+      </Header>
+      <Content>
+        <Card className="m-4">
+          <Formik
+            initialValues={{
+              photo: null,
+              name: '',
+              phone_number: '',
+              address: '',
+              google_maps_link: '',
+              parking_availability: null,
+              parking_size: 0,
+              start_of_work: null,
+              end_of_work: null,
+            }}
+            validationSchema={clinicSchema}
+            onSubmit={(values) => {
+              dispatch(updateClinic(values));
+            }}
+          >
+            {({ dirty, isValid, values, handleSubmit }) => (
+              <Form
+                layout="vertical"
+                name="clinic-form"
+                onSubmit={handleSubmit}
+              >
+                <Row justify="center" className="mb-5 mt-4">
+                  <Col span={6}>
+                    <Field component={FormImageUpload} name={'photo'}></Field>
+                  </Col>
+                </Row>
 
-            <Row>
-              <Col span={24}>
-                <Field
-                  component={FormField}
-                  label={formatMessage(messages.clinic_name)}
-                  name={'name'}
-                  errorTexts={{
-                    label: formatMessage(messages.error_input_label_name),
-                  }}
-                />
-              </Col>
-            </Row>
-            <Row gutter={16}>
-              <Col span={8}>
-                <Field
-                  component={FormField}
-                  label={formatMessage(messages.phone_number)}
-                  name={'phone_number'}
-                  errorTexts={{
-                    label: formatMessage(
-                      messages.error_input_label_phone_number
-                    ),
-                  }}
-                />
-              </Col>
-              <Col span={16}>
-                <Field
-                  component={FormField}
-                  label={formatMessage(messages.address)}
-                  name={'address'}
-                  errorTexts={{
-                    label: formatMessage(messages.error_input_label_address),
-                  }}
-                />
-              </Col>
-            </Row>
-            <Row>
-              <Col span={24}>
-                <Field
-                  component={FormField}
-                  label={formatMessage(messages.google_maps_link)}
-                  name={'google_maps_link'}
-                  errorTexts={{
-                    label: formatMessage(
-                      messages.error_input_label_google_maps_link
-                    ),
-                  }}
-                />
-              </Col>
-            </Row>
-            <Row>
-              <Col span={18}>
-                <Form.Item
-                  name="radio-group"
-                  label={formatMessage(messages.parking_availability)}
-                >
-                  <Radio.Group
-                    onChange={(event) => {
-                      setVisibility(event.target.value === AVAILABLE);
-                      values.parking_availability = event.target.value;
-                    }}
-                  >
-                    <Radio value={NO}>
-                      {formatMessage(messages.parking_no)}
-                    </Radio>
-                    <Radio value={FREE}>
-                      {formatMessage(messages.parking_free)}
-                    </Radio>
-                    <Radio value={AVAILABLE}>
-                      {formatMessage(messages.parking_available)}
-                    </Radio>
-                  </Radio.Group>
-                </Form.Item>
-              </Col>
-              <Col span={6}>
-                {visibilityOfParkinSizeField ? (
-                  <Form.Item label={formatMessage(messages.parking_size)}>
-                    <Field
-                      component={FormInputField}
-                      name={'parking_size'}
-                      type={'number'}
-                      min={1}
-                    />
-                  </Form.Item>
-                ) : null}
-              </Col>
-            </Row>
-            <Form.Item label={formatMessage(messages.working_hours)}>
-              <Row gutter={8}>
-                <Col xs={10} md={6}>
-                  <Field
-                    component={FormInputField}
-                    name={'start_of_work'}
-                    type={'time'}
-                  />
-                </Col>
-                <Col md={4} className="text-center">
-                  <MinusOutlined className="mt-3 text-primary" />
-                </Col>
-                <Col xs={10} md={6}>
-                  <Field
-                    component={FormInputField}
-                    name={'end_of_work'}
-                    type={'time'}
-                  />
-                </Col>
-              </Row>
-            </Form.Item>
+                <Row justify="center">
+                  <Col span={6}>
+                    <Title type="secondary" level={2} className="mt-4">
+                      Clinic Details
+                    </Title>
+                  </Col>
+                  <Col span={12}>
+                    <Row>
+                      <Col span={24}>
+                        <Field
+                          component={FormField}
+                          label={formatMessage(messages.clinic_name)}
+                          name={'name'}
+                          errorTexts={{
+                            label: formatMessage(
+                              messages.error_input_label_name
+                            ),
+                          }}
+                        />
+                      </Col>
+                    </Row>
+                    <Row gutter={16}>
+                      <Col span={8}>
+                        <Field
+                          component={FormField}
+                          label={formatMessage(messages.phone_number)}
+                          name={'phone_number'}
+                          errorTexts={{
+                            label: formatMessage(
+                              messages.error_input_label_phone_number
+                            ),
+                          }}
+                        />
+                      </Col>
+                      <Col span={16}>
+                        <Field
+                          component={FormField}
+                          label={formatMessage(messages.address)}
+                          name={'address'}
+                          errorTexts={{
+                            label: formatMessage(
+                              messages.error_input_label_address
+                            ),
+                          }}
+                        />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col span={24}>
+                        <Field
+                          component={FormField}
+                          label={formatMessage(messages.google_maps_link)}
+                          name={'google_maps_link'}
+                          errorTexts={{
+                            label: formatMessage(
+                              messages.error_input_label_google_maps_link
+                            ),
+                          }}
+                        />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col span={24}>
+                        <Form.Item
+                          name="radio-group"
+                          label={formatMessage(messages.parking_availability)}
+                        >
+                          <Radio.Group
+                            onChange={(event) => {
+                              setVisibility(event.target.value === AVAILABLE);
+                              values.parking_availability = event.target.value;
+                            }}
+                          >
+                            <Radio value={NO}>
+                              {formatMessage(messages.parking_no)}
+                            </Radio>
+                            <Radio value={FREE}>
+                              {formatMessage(messages.parking_free)}
+                            </Radio>
+                            <Radio value={AVAILABLE}>
+                              {formatMessage(messages.parking_available)}
+                            </Radio>
+                          </Radio.Group>
+                        </Form.Item>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col span={6}>
+                        {visibilityOfParkinSizeField ? (
+                          <Form.Item
+                            label={formatMessage(messages.parking_size)}
+                          >
+                            <Field
+                              component={FormInputField}
+                              name={'parking_size'}
+                              type={'number'}
+                              min={1}
+                            />
+                          </Form.Item>
+                        ) : null}
+                      </Col>
+                    </Row>
+                    <Form.Item label={formatMessage(messages.working_hours)}>
+                      <Row gutter={8}>
+                        <Col span={6}>
+                          <Field
+                            component={FormInputField}
+                            name={'start_of_work'}
+                            type={'time'}
+                          />
+                        </Col>
+                        <Col span={2} className="text-center">
+                          <MinusOutlined className="mt-3 text-primary" />
+                        </Col>
+                        <Col span={6}>
+                          <Field
+                            component={FormInputField}
+                            name={'end_of_work'}
+                            type={'time'}
+                          />
+                        </Col>
+                      </Row>
+                    </Form.Item>
 
-            <Row>
-              <Col>
-                <Form.Item>
-                  <Button
-                    name="create"
-                    type="primary"
-                    disabled={!dirty || !isValid}
-                    onClick={() => handleSubmit(values)}
-                  >
-                    {formatMessage(messages.create)}
-                  </Button>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-        )}
-      </Formik>
-    </Card>
+                    <Row>
+                      <Col>
+                        <Form.Item>
+                          <Button
+                            name="create"
+                            type="primary"
+                            disabled={!dirty || !isValid}
+                            onClick={() => handleSubmit(values)}
+                          >
+                            {formatMessage(messages.create)}
+                          </Button>
+                        </Form.Item>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Form>
+            )}
+          </Formik>
+        </Card>
+      </Content>
+    </Layout>
   );
 };
 
