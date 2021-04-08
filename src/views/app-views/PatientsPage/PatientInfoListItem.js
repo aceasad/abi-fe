463357c2a71ef0c6fb4b3cf3ce51da12c6915267
Slobodan Subotic <slobodan.subotic@vivifyideas.com>
2021-04-08 16:12:46 +0200

    #FOEK-84  - patient info fields corrections.

--- src/views/app-views/PatientsPage/PatientInfoListItem.js
+++ src/views/app-views/PatientsPage/PatientInfoListItem.js
@@ -7,8 +7,8 @@ const { Text } = Typography;
 const PatientInfoListItem = ({ children }) => {
   return (
     <Flex justifyContent="between" className="mb-2">
-      <Text className="mr-4">{children[0]}</Text>
-      <Text>{children[1]}</Text>
+      <Text className="mr-4">{children[0]}:</Text>
+      <Text className="text-right text-break">{children[1]}</Text>
     </Flex>
   );
 };
