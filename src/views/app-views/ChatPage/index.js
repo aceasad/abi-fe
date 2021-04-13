f0d79c9dd5ef4caa5bb0e224cc402c9a15bbe9e6
Slobodan Subotic <slobodan.subotic@vivifyideas.com>
2021-04-13 13:33:03 +0200

    #FOEK-153 - added page header on conversations page.

--- src/views/app-views/ChatPage/index.js
+++ src/views/app-views/ChatPage/index.js
@@ -2,18 +2,33 @@ import React from 'react';
 import InnerAppLayout from 'layouts/inner-app-layout';
 import ChatContent from './ChatContent';
 import ChatMenu from './ChatMenu';
+import { Button, PageHeader } from 'antd';
+import { useIntl } from 'react-intl';
+import messages from './messages';
 
 const Chat = (props) => {
+  const { formatMessage } = useIntl();
   return (
-    <div className="chat">
-      <InnerAppLayout
-        sideContent={<ChatMenu {...props} />}
-        mainContent={<ChatContent {...props} />}
-        sideContentWidth={450}
-        sideContentGutter={false}
-        border
+    <>
+      <PageHeader
+        className="p-0 mb-4"
+        title={formatMessage(messages.conversationsTitle)}
+        extra={[
+          <Button type="primary">
+            {formatMessage(messages.conversationsMassInvites)}
+          </Button>,
+        ]}
       />
-    </div>
+      <div className="chat">
+        <InnerAppLayout
+          sideContent={<ChatMenu {...props} />}
+          mainContent={<ChatContent {...props} />}
+          sideContentWidth={450}
+          sideContentGutter={false}
+          border
+        />
+      </div>
+    </>
   );
 };
 
