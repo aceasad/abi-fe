3af4950ef400a3eb6b6ccf2c802138a321cb1b3f
ap <antonispopovidis@gmail.com>
2021-11-15 09:59:01 +0200

    - add more url prefix handling to account for login/logout cases

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
-import { AUTH_PREFIX_PATH, APP_PAGES_PREFIX_PATH } from './configs/AppConfig';
+import {
+  URL_PREFIX_PATH,
+  AUTH_PREFIX_PATH,
+  APP_PAGES_PREFIX_PATH,
+} from './configs/AppConfig';
 
 export const ROUTES = {
   LOGIN: `${AUTH_PREFIX_PATH}/login`,
   FORGOT_PASSWORD: `${AUTH_PREFIX_PATH}/forgot-password`,
   CONTACTS: `${APP_PAGES_PREFIX_PATH}/contacts`,
-  DASHBOARD: `/`,
+  DASHBOARD: `${URL_PREFIX_PATH}/`,
   CREATE_PASSWORD: `${AUTH_PREFIX_PATH}/create-password`,
   FIRST_CLINIC_UPDATE: `${APP_PAGES_PREFIX_PATH}/first-clinic-update`,
 };
