8e577cb6a37e72222739ac64400f9b324a9d179b
Asad Saeed <asad.saeed@live.com>
2023-08-11 19:01:28 +0500

    Updated frontend

--- src/containers/Forms/ClinicForm/ClinicForm.js
+++ src/containers/Forms/ClinicForm/ClinicForm.js
@@ -34,6 +34,8 @@ const ClinicForm = ({ clinicData = null, showSuccess, showError }) => {
     clinicData?.start_of_work === '00:00:00' &&
       clinicData?.end_of_work === '00:00:00'
   );
+  const { isPasIntegrated } = useSelector(state => state.auth.user);
+
   const location = useLocation();
 
   const editClinicSlug = '/edit-clinic';
@@ -49,7 +51,8 @@ const ClinicForm = ({ clinicData = null, showSuccess, showError }) => {
     if (!values.photo) {
       formData.append('photo', '');
     }
-
+    formData.delete('isPasIntegrated')
+    formData.append('isPasIntegrated',isPasIntegrated)
     if (clinicData) {
       dispatch(
         updateClinic({
@@ -91,6 +94,8 @@ const ClinicForm = ({ clinicData = null, showSuccess, showError }) => {
           parking_size: clinicData?.parking_size || 0,
           start_of_work: clinicData?.start_of_work || initialWorkTime,
           end_of_work: clinicData?.end_of_work || initialWorkTime,
+          PasAPIEndpoint: clinicData?.PasAPIEndpoint || '',
+          isPasIntegrated: clinicData?.isPasIntegrated || isPasIntegrated,
         }}
         enableReinitialize
         validationSchema={clinicSchema}
