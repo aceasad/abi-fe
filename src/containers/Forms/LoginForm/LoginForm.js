bfcbb4b9fc15d818034b7fa7aaa93e5933b5073b
Asad Saeed <asad.saeed@live.com>
2024-05-01 22:03:14 +0500

    Added ssl to dev frontend, google recapture and adjusted env variables

--- src/containers/Forms/LoginForm/LoginForm.js
+++ src/containers/Forms/LoginForm/LoginForm.js
@@ -141,7 +141,7 @@ export const LoginForm = () => {
               labelBlock={true}
             />
             <Form.Item className = 'mt-sm-5 ml-sm-4'>
-              <ReCaptcha siteKey={'6Lf2NrcpAAAAABZn_Lw6SydK01wORSAJmXCWZ6-O'} callback={handleReToken} />
+              <ReCaptcha siteKey={'6Lc5-MwpAAAAAFJnhYI3lqTIwhFJ6DPTu3L20o_x'} callback={handleReToken} />
             </Form.Item>
 
             <Form.Item className="mt-sm-5">
