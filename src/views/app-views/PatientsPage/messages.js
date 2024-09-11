98fa958e35d9353e6309126609591081d77ea48b
Asad Saeed <asad.saeed@live.com>
2024-09-11 18:08:05 +0500

    added error message on incorrect nhs number

--- src/views/app-views/PatientsPage/messages.js
+++ src/views/app-views/PatientsPage/messages.js
@@ -95,6 +95,10 @@ export default defineMessages({
     id: `${detailScope}.error.email_already_exists`,
     defaultMessage: 'Email is already taken',
   },
+  nhsNumberIncorrect: {
+    id: `${detailScope}.error.incorrect_external_identification_number`,
+    defaultMessage: 'NHS number is incorrect!',
+  },
   dateOfBirth: {
     id: `${detailScope}.side.form.date_of_birth`,
     defaultMessage: 'Date of birth',
