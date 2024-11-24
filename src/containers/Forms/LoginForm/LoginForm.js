bf23a0b1a22a0c8a2073cd882b73963190c740d8
Asad Saeed <asad.saeed@live.com>
2024-11-24 11:37:09 +0500

    Added new recapture google keys

--- src/containers/Forms/LoginForm/LoginForm.js
+++ src/containers/Forms/LoginForm/LoginForm.js
@@ -150,7 +150,7 @@ export const LoginForm = () => {
               labelBlock={true}
             />
             <Form.Item className = 'mt-sm-5 ml-sm-4'>
-              <ReCaptcha siteKey={'6Lf2NrcpAAAAABZn_Lw6SydK01wORSAJmXCWZ6-O'} callback={handleReToken} />
+              <ReCaptcha siteKey={'6Lcoc4gqAAAAAJ_mhsJyfgAr7CbSlO9l62_crs5G'} callback={handleReToken} />
             </Form.Item>
 
             <Form.Item className="mt-sm-5">
